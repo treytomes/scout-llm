63119227acf8dab2b4882adf6455be5fc8d1fb8b
Trey Tomes <trey.tomes@gmail.com>
2026-04-14 16:48:54 -0500

    chore: refactor data folder

--- src/server/config.py
+++ src/server/config.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 WEB_DIR = Path(__file__).parent.parent / "web"
 
-DATA_ROOT = Path("./datasets")
+DATA_ROOT = Path("./data/datasets")
 DATASET_FILE = DATA_ROOT / "datasets.json"
